d35956e9241bc71be9802aa64485b5c51e7d9fab
Artem Antonian <art902004@gmail.com>
2025-12-08 13:17:58 +0300

    wre

---
+++ Lection1_JSON/marshal/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+)
+
+type Professor struct {
+	Name       string     `json:"name"`
+	ScienceID  int        `json:"science_id"`
+	IsWorking  bool       `json:"is_working"`
+	University University `json:"university"`
+}
+type University struct {
+	Name string `json:"name"`
+	City string `json:"city"`
+}
+
+func main() {
+	prof1 := Professor{
+		Name:      "Petr",
+		ScienceID: 8237917,
+		IsWorking: true,
+		University: University{
+			Name: "BMSTU",
+			City: "Москва",
+		},
+	}
+	//1. Превратим профессора в последовательность байтов
+	byteArr, err := json.MarshalIndent(prof1, "", " ")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(string(byteArr))
+	err = os.WriteFile("output.json", byteArr, 0666) //-rw-rw-rw-
+	if err != nil {
+		log.Fatal(err)
+	}
+}
