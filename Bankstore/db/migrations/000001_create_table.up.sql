edb17797837402ccc7f0b44c28361ca32f837d33
Artem Antonian <art902004@gmail.com>
2025-12-09 16:38:27 +0300

    23

--- Bankstore/db/migrations/000001_create_table.up.sql
+++ Bankstore/db/migrations/000001_create_table.up.sql
@@ -3,7 +3,7 @@ CREATE TYPE Currency As ENUM (
     'EUR'
 );
 
-CREATE TABLE "account" (
+CREATE TABLE "accounts" (
     "id" bigserial PRIMARY KEY,
     "owner" varchar NOT NULL,
     "balance" bigint NOT NULL,
@@ -36,12 +36,12 @@ CREATE INDEX ON "transfers" ("to_account_id");
 
 CREATE INDEX ON "transfers" ("from_account_id", "to_account_id");
 
-COMMENT ON COLUMN "entries"."amount" IS "can be negative or positive";
+COMMENT ON COLUMN "entries"."amount" IS 'can be negative or positive';
 
-COMMENT ON COLUMN "transfers"."amount" IS "must be positive";
+COMMENT ON COLUMN "transfers"."amount" IS 'must be positive';
 
-ALTER TABLE "entries" ADD FOREIGN KEY ("account_id") REFERENCES "account" ("id");
+ALTER TABLE "entries" ADD FOREIGN KEY ("account_id") REFERENCES "accounts" ("id");
 
-ALTER TABLE "transfers" ADD FOREIGN KEY ("from_account_id") REFERENCES "account" ("id");
+ALTER TABLE "transfers" ADD FOREIGN KEY ("from_account_id") REFERENCES "accounts" ("id");
 
-ALTER TABLE "transfers" ADD FOREIGN KEY ("to_account_id") REFERENCES "account" ("id");
+ALTER TABLE "transfers" ADD FOREIGN KEY ("to_account_id") REFERENCES "accounts" ("id");
